4949c0cffd94b2959f28beb4eb66331d7c2b2ad5
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-03-17 05:19:12 +0700

    feat : add extract css plugin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { hello } from "./other";
 import Mantap from "./source";
 import "./index.css";
+import "./other.scss";
 
 console.log(`This is Index `);
 hello(`Fadhil`);
 
+let myArr : number[] = [1,2,3,4];
+
 Mantap();
+
+
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,55 +1,77 @@
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-const config : webpack.Configuration = {
-    context : import.meta.dirname + "/src",
-    mode : "production",
-    //multiple main entry
-    //entry : ["./index.ts","./index2.ts"],  
-    //object multiple  entry 
-    entry : {
-        "main" : {
-            import : "./index.ts",
-            dependOn : "together"
-        },
-        "other" : {
-            import : "./index2.ts",
-            dependOn : "together"
-        },
-        "together" : {
-            import : "./source.ts",
-            filename : "addtogether",
-            //runtime : "a1"
-        }
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const config: webpack.Configuration = {
+  context: import.meta.dirname + "/src",
+  mode: "production",
+  //multiple main entry
+  //entry : ["./index.ts","./index2.ts"],
+  //object multiple  entry
+  entry: {
+    main: {
+      import: "./index.ts",
+      dependOn: "together",
     },
-    output : {
-        asyncChunks : true,
-        path : import.meta.dirname + "/dist",
-        filename : "[name]-[contenthash].bundle.js",
+    other: {
+      import: "./index2.ts",
+      dependOn: "together",
     },
-    module : {
-        rules : [
-            {
-                test : /\.tsx?$/,
-                use : "ts-loader",
-                exclude : /node_modules/,
-            },
-            {
-                test : /\.css$/,
-                use : [
-                   {
-                    loader : "style-loader"
-                   },
-                   {
-                    loader : "css-loader"
-                   }
-                ],
-                exclude : /node_modules/,
-            }
-        ]
+    together: {
+      import: "./source.ts",
+      filename: "addtogether",
+      //runtime : "a1"
     },
-    resolve : {
-        extensions : [".tsx",".ts",".js"]
-    },
-    plugins : [new HtmlWebpackPlugin({template : "./index.html"}), new webpack.ProgressPlugin()]
-}
+  },
+  output: {
+    asyncChunks: true,
+    path: import.meta.dirname + "/dist",
+    filename: "[name]-[contenthash].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
 export default config;
